7bf6604eb9efd446b357905fb9796415bd5950d7
mugi <munkhuu1233@gmail.com>
2021-04-06 12:13:31 -0400

    added scripts to server package.json

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern-starter", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,6 +26,12 @@ app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
+});
+
+app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, () => {
