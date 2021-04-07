6223932b3d37f00c2231073827caa43166e8f203
mugi <munkhuu1233@gmail.com>
2021-04-07 16:31:57 -0400

    added bootstrap cdn and added navbar

---
+++ client/src/Navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const navbar = () => {
+  return (
+    <div>
+     
+<nav className="navbar navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+
+    </div>
+  );
+};
+
+export default navbar;
